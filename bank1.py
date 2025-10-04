70f8d104fce74725f423b0cf3a38762148836875
Soumya-ui-bytes <soumyarajagopal027@gmail.com>
2025-10-04 11:23:51 +0530

    saved

--- bank1.py
+++ bank1.py
@@ -1,3 +1,12 @@
-bal= int(input("Enter the balance: "))
-deposit= int(input("Enter the deposit amount: "))
-print(f"the updated balance : {bal}")
+bal = int(input("Enter the balance: "))
+
+deposit = int(input("Enter the deposit amount: "))
+bal += deposit
+print(f"Balance after deposit: {bal}")
+
+withdraw = int(input("Enter the withdrawal amount: "))
+if withdraw <= bal:
+    bal -= withdraw
+    print(f"Withdrawal successful. New balance: {bal}")
+else:
+    print("Insufficient balance.")
